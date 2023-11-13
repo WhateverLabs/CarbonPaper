4f146c4461b67da7349677a31a1061e0ca6ebb1e
httpjamesm <github@httpjames.space>
2023-11-13 00:41:42 -0500

    feat: homepage

---
+++ src/lib/components/ColourfulCarbonContainer.svelte
@@ -0,0 +1,30 @@
+<div class="container">
+	<slot />
+</div>
+
+<style lang="scss">
+	$borderWidth: 12px;
+
+	.container {
+		background: black;
+		background-clip: padding-box;
+		border: $borderWidth solid transparent;
+		position: relative;
+
+		padding: 1rem;
+		border-radius: 20px;
+		box-shadow: 2px 2.5px 0.4px 1px rgba(0, 0, 0, 0.2);
+
+		&::after {
+			position: absolute;
+			background: linear-gradient(0deg, #da3526, #4a26da);
+			top: -$borderWidth;
+			bottom: -$borderWidth;
+			left: -$borderWidth;
+			right: -$borderWidth;
+			content: '';
+			z-index: -1;
+			border-radius: 15px;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+	import ColourfulCarbonContainer from '$lib/components/ColourfulCarbonContainer.svelte';
+</script>
+
+<div class="wrapper">
+	<div class="container">
+		<div class="hero">
+			<h1>Invisible Ink for the Digital Age</h1>
+			<ColourfulCarbonContainer>
+				<p style="margin: 0; text-align: left; font-size: 1.2rem">
+					Carbon Paper encrypts your pastes with XSalsa20-Poly1305, a battle-tested algorithm that
+					keeps your secrets safe.
+					<br />
+					<br />
+					All of this is done on the client-side, ensuring that no one but you and the receivers of the
+					share link can view the paste's contents.
+				</p>
+			</ColourfulCarbonContainer>
+		</div>
+
+		<div class="simplicity">
+			<h1>Simplicity is Key</h1>
+			<p>Just paste, copy the link, and share it with whoever.</p>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		padding: 1rem;
+		box-sizing: border-box;
+
+		.container {
+			max-width: 600px;
+			text-align: center;
+			color: white;
+			width: 100%;
+
+			.simplicity {
+				margin-top: 5rem;
+			}
+		}
+	}
+</style>
