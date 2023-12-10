cabcb860b36193fb890fec54c887bf24b47ed253
httpjamesm <github@httpjames.space>
2023-12-09 22:51:25 -0500

    fix: colourful carbon container border

--- src/lib/components/ColourfulCarbonContainer.svelte
+++ src/lib/components/ColourfulCarbonContainer.svelte
@@ -1,30 +1,25 @@
 <div class="container">
-	<slot />
+	<div class="child">
+		<slot />
+	</div>
 </div>
 
 <style lang="scss">
-	$borderWidth: 12px;
-
 	.container {
-		background: black;
-		background-clip: padding-box;
-		border: $borderWidth solid transparent;
+		background: linear-gradient(0deg, #da3526, #4a26da);
 		position: relative;
 
-		padding: 1rem;
+		padding: 0.5rem;
 		border-radius: 20px;
 		box-shadow: 2px 2.5px 0.4px 1px rgba(0, 0, 0, 0.2);
 
-		&::after {
-			position: absolute;
-			background: linear-gradient(0deg, #da3526, #4a26da);
-			top: -$borderWidth;
-			bottom: -$borderWidth;
-			left: -$borderWidth;
-			right: -$borderWidth;
-			content: '';
-			z-index: -1;
-			border-radius: 15px;
+		.child {
+			width: 100%;
+			height: 100%;
+			background-color: black;
+			border-radius: 10px;
+			padding: 1rem;
+			box-sizing: border-box;
 		}
 	}
 </style>
