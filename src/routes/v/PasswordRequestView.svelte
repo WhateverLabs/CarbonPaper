0607ea74644b79449ac6d15b406978eca4f9282b
httpjamesm <github@httpjames.space>
2023-12-09 23:11:27 -0500

    style: mobile width for pw request view

--- src/routes/v/PasswordRequestView.svelte
+++ src/routes/v/PasswordRequestView.svelte
@@ -8,9 +8,22 @@
 
 <h1>Enter the paste's password</h1>
 
-<form on:submit|preventDefault={onSubmit}>
+<form on:submit|preventDefault={onSubmit} class="password-form">
 	<CarbonInput id="password-input" label="Password" bind:value={password} secureText={true} />
-	<div style="display: flex; justify-content: center; width: 100%; margin-top: 1rem;">
+	<div class="decrypt-container">
 		<CarbonButton>Decrypt</CarbonButton>
 	</div>
 </form>
+
+<style lang="scss">
+	.password-form {
+		display: contents;
+	}
+
+	.decrypt-container {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		margin-top: 1rem;
+	}
+</style>
